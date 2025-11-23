db3c617cf49d31a2bd586d3855da0986914e3f58
jagjitphull <jagjit.phull@gmail.com>
2025-11-24 01:07:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "aws_instance" "demo" {
 
   # Root volume configuration
   root_block_device {
-    volume_size           = 31
+    volume_size           = 30
     volume_type           = "gp3"
     delete_on_termination = true
     encrypted             = true
